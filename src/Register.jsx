1b4717f9d2d7cb99c168ad204a04ad1efe29fc12
鄭至盛 <sam22187212@gmail.com>
2023-06-10 16:04:42 +0800

    test

---
+++ src/Register.jsx
@@ -0,0 +1,11 @@
+function Register() {
+    return (
+        <div className="Register">
+        <h1>
+            Register Page
+        </h1>
+        </div>
+    );
+}
+
+export default Register;
